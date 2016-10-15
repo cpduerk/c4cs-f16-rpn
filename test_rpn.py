01549522504c8e45e65d046c5edfd84219d7ec3a
Christian Duerk <cpduerk@umich.edu>
2016-10-15 00:05:42 -0400

    Added exponentiation test

--- test_rpn.py
+++ test_rpn.py
@@ -18,4 +18,6 @@ class TestBasics(unittest.TestCase):
 	def test_divide(self):
 		result = rpn.calculate("8 4 /")
 		self.assertEqual(2, result)
-
+	def test_exp(self):
+		result = rpn.calculate("2 3 ^")
+		self.assertEqual(8, result)
